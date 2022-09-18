321f529e397e5443724fd4b0a80fd2dc3217334d
Vadim <vadim192000@gmail.com>
2022-09-18 23:18:53 +0300

    обновление данных с сервера (карточки и рандомный герой)

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -12,10 +12,10 @@ class MarvelService {
         return await res.json();
     }
 
-    // getAllCharacters = async () => {
-    //     const res = await this.getResource(`${this._apiBase}characters?${this._apiKey}`);
-    //     return res.data.results.map(this._transformCharacter);
-    // }
+    getAllCharacters = async () => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter);
+    }
 
     getCharacter = async (id) => {
         const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
@@ -25,11 +25,13 @@ class MarvelService {
     _transformCharacter = (char) => {
         
         return {
+            id: char.id,
             name: char.name,
             description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
             thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
             homepage: char.urls[0].url,
-            wiki: char.urls[1].url
+            wiki: char.urls[1].url,
+            comics: char.comics.items
         }
     }
 }
