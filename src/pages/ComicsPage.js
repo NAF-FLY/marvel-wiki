a6b8be7f813e1572f7fe1f15774908ee7ceaab52
Vadim <vadim192000@gmail.com>
2022-10-04 22:54:51 +0300

    SEO-оптимизация с помощью Helmet

--- src/pages/ComicsPage.js
+++ src/pages/ComicsPage.js
@@ -1,9 +1,14 @@
 import ComicsList from "../components/comicsList/ComicsList";
 import AppBanner from "../components/appBanner/AppBanner";
+import { Helmet } from "react-helmet";
 
 function ComicsPage() {
   return (
     <>
+      <Helmet>
+        <meta name="description" content="Page with list of our comics" />
+        <title>Marvel comics page</title>
+      </Helmet>
       <AppBanner />
       <ComicsList />
     </>
